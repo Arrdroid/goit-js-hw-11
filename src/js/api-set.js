c05449c6cfafd21666ddf9c6366527e9afbc81b0
Arrdroid <alexmirensko@gmail.com>
2023-08-24 14:51:39 +0300

    update

--- src/js/api-set.js
+++ src/js/api-set.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const URL = "https://pixabay.com/api/";
 const API_KEY = "38931090-55c6b7c2dc99483da7553cf12";
-const ONPAGE = 40;
+export const ONPAGE = 40;
 
 export async function serviceRequest(value, page) {
   axios.defaults.params = {
